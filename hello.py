2871359ba397ee44705d0e6ecb569f8ff0e1a954
Mark Williams <mark@madronacreekranch.com>
2014-12-26 15:13:36 -0600

    added hello.py

---
+++ hello.py
@@ -0,0 +1,19 @@
+#=========================================================================
+#
+# Python Source File -- Created with SAPIEN Technologies PrimalScript 2009
+#
+# NAME: 
+#
+# AUTHOR: Admin , 
+# DATE  : 12/26/2014
+#
+# COMMENT: 
+#
+#=========================================================================
+
+import time
+
+def hello():
+	print 'Hello World'
+	sleep (30)
+hello()
