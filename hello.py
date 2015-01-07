72190f0754017fa2ecf281d11b43fd732a2f0b80
Mark R Williams <mark@madronacreekranch.com>
2015-01-06 20:46:38 -0600

    Led flash added

--- hello.py
+++ hello.py
@@ -2,11 +2,30 @@
 import time
 import RPi.GPIO as GPIO
 
+# Set the pin numbering mode
+GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)
+
+#name the pin
+led = 12
+
+#set up the pin
+GPIO.setup(led, GPIO.OUT)
 
 def hello():
 
     print 'Hello Mark!'
-    time.sleep (30)
+    GPIO.output(led, 1)
+    time.sleep (5)
+    print 'Goodbye Mark!'
+    GPIO.output(led, 0)
+    time.sleep (5)
 
-while True:
+#loop
+count = 0
+countmax = 10
+while count < countmax:
     hello()
+    count +=1
+
+GPIO.cleanup()
